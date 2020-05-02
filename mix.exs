131054cfcf38ad42cbe848436bbff0ef63c377c6
Ivan Youroff <ivan.youroff@gmail.com>
2020-05-02 19:06:31 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoCursor.MixProject do
 
   def project, do: [
     app: :ecto_cursor,
-    version: "0.1.0",
+    version: "0.1.1",
     elixir: "~> 1.10",
     deps: deps(),
     description: description(),
